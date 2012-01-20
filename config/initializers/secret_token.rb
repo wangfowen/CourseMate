8c1fe0d9661d05d0736b973cd98d23b196718a85
Owen Wang <wangfowen@gmail.com>
2012-01-20 18:12:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursemate::Application.config.secret_token = 'abfa3be335f5b99e86acfff94239a8e94d66d5fc9387ec623bf13366d3ea6913acc2eb1e64aa74a2a8f18aff811271c017007a71720fc3789047026e1d4946f4'
