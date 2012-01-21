19cea84e4b36adef04b1bcb857090ede1c0d16b8
Owen Wang <wangfowen@gmail.com>
2012-01-20 20:20:37 -0500

    adding koala gem for fb auth

--- app/controllers/frontends_controller.rb
+++ app/controllers/frontends_controller.rb
@@ -1,7 +1,11 @@
 class FrontendsController < ApplicationController
+
+  #before_filter :authenticate_user
+
   # GET /frontends
   # GET /frontends.json
   def index
+    #@graph = Koala::Facebook::API.new(@access_token)
     @display = "Hello app"
     respond_to do |format|
       format.html # index.html.erb
